f5b3f5ecd2ae514786f0dea3bea2ba36b4189f29
dany14101 <danylinan74@gmail.com>
2024-11-27 03:19:26 +0000

    cambio 16.2

--- Main.c
+++ Main.c
@@ -352,7 +352,7 @@ void men_dat(FILE *arch)
 	{
 		printf("1. Agregar dato\n");
 		printf("2. Eliminar dato\n");
-		printf("3. Modificar entidad\n");
+		printf("3. Modificar dato\n");
 		printf("4. volver al menu\n");
 		printf("Dame que opcion quieres:");
 		scanf("%d",&op);
@@ -1034,7 +1034,7 @@ void elimina_datos(FILE *arch,char enti[])
 	int val,op,ta=0;
 	float val1;
 	char val2[120]="";
-	long val3;
+	long val3,prueb;
 	long arr[200];
 	bool val4;
 //
@@ -1048,6 +1048,7 @@ void elimina_datos(FILE *arch,char enti[])
 		printf("No hay entidades a la cual agregar atributos\n");
 		return;
 	}
+	//Le da las opciones de los datos que puede eliminar
 		while(cab!=-1)
 		{	
 			fseek(arch, cab, SEEK_SET);
@@ -1058,10 +1059,10 @@ void elimina_datos(FILE *arch,char enti[])
 				aux=entidad.puntatri;
 				fin=entidad.pundata;
 				fin1=fin;
-				printf("%d)",ta);
 				//Va a buscar dato en orden
 				while(fin!=-1)
 				{
+					printf("%d)",ta);
 					fseek(arch,aux,SEEK_SET);
 					while(fread(&atrib,sizeof(Atri),1,arch)==1)
 					{
@@ -1108,19 +1109,233 @@ void elimina_datos(FILE *arch,char enti[])
 					}
 					fseek(arch,fin1,SEEK_SET);
 					fin1=ftell(arch);
+					arr[ta]=fin1;
 					fread(&fin,sizeof(long),1,arch);
-					arr[ta]=fin;
+					fin1=fin;
 					ta++;
 					printf("\n");
 				}
 				printf("Dame que elemento quieres eliminar:");
 				scanf("%d",&op);	
+				if(op==0&&ta-1==0)
+				{
+					fseek(arch,cab+offsetof(Enti,pundata),SEEK_SET);
+					prueb=ftell(arch);
+					fwrite(&constante,sizeof(long),1,arch);
+					return;
+				}
+				else
+				{
+					fseek(arch,arr[op-1],SEEK_SET);
+					fwrite(&arr[op],sizeof(long),1,arch);
+					return;
+				}
 			}
 			cab=entidad.puntsig;
 		}
+		printf("No se encontro\n");
 }
 
 void modifica_datos(FILE *arch,char enti[])
 {
-	
+	//Variables para el primer caso 
+	Atri aux3;
+	int val,op,ta=0;
+	float val1;
+	char val2[120]="";
+	long val3,prueb;
+	long arr[200];
+	bool val4;
+//
+	long cab,valant=-1,pos,ini,aux,fin,fin1,constante=-1;
+	Atri atrib;
+	Enti entidad;
+	fseek(arch, 0, SEEK_SET);
+	fread(&cab,sizeof(long),1,arch);
+	if(cab==-1)
+	{
+		printf("No hay entidades a la cual agregar atributos\n");
+		return;
+	}
+	//Le da las opciones de los datos que puede eliminar
+		while(cab!=-1)
+		{	
+			fseek(arch, cab, SEEK_SET);
+			fread(&entidad,sizeof(Enti),1,arch);
+			if(strcmp(enti,entidad.nom)==0)
+			{	
+				valant=-1;
+				aux=entidad.puntatri;
+				fin=entidad.pundata;
+				fin1=fin;
+				//Va a buscar dato en orden
+				while(fin!=-1)
+				{
+					printf("%d)",ta);
+					fseek(arch,aux,SEEK_SET);
+					while(fread(&atrib,sizeof(Atri),1,arch)==1)
+					{
+						if(atrib.tipo=='i')
+						{
+							fseek(arch,fin1,SEEK_SET);
+							fread(&val,sizeof(int),1,arch);
+							printf("%d-",val);
+							fin1=fin1+sizeof(int);
+						}
+						if(atrib.tipo=='f')
+						{
+							fseek(arch,fin1,SEEK_SET);
+							fread(&val1,sizeof(float),1,arch);
+							printf("%.2f-",val1);
+							fin1=fin1+sizeof(float);
+						}
+						if(atrib.tipo=='c')
+						{
+							fseek(arch,fin1,SEEK_SET);
+							fread(&val2,sizeof(val2),1,arch);
+							printf("%s-",val2);
+							fin1=fin1+sizeof(val2);
+						}
+						if(atrib.tipo=='l')
+						{
+							fseek(arch,fin1,SEEK_SET);
+							fread(&val3,sizeof(long),1,arch);
+							printf("%ld-",val3);
+							fin1=fin1+sizeof(long);
+						}
+						if(atrib.tipo=='b')
+						{
+							fseek(arch,fin1,SEEK_SET);
+							fread(&val4,sizeof(bool),1,arch);
+							printf("%d-",val4);
+							fin1=fin1+sizeof(bool);
+						}
+						if(atrib.puntsig==-1)
+						{
+							break;
+						}
+					fseek(arch,atrib.puntsig,SEEK_SET);
+					}
+					fseek(arch,fin1,SEEK_SET);
+					fin1=ftell(arch);
+					arr[ta]=fin1;
+					fread(&fin,sizeof(long),1,arch);
+					fin1=fin;
+					ta++;
+					printf("\n");
+				}
+				printf("Dame que elemento quieres modificar:");
+				scanf("%d",&op);	
+				if(op==0&&ta-1==0)
+				{
+					fseek(arch,entidad.puntatri,SEEK_SET);
+					prueb=entidad.pundata;
+					while(fread(&atrib,sizeof(Atri),1,arch)==1)
+					{
+						fseek(arch,prueb,SEEK_SET);
+						pos=ftell(arch);
+						if(atrib.tipo=='i')
+						{
+							printf("Dame el valor entero:");
+							scanf("%d",&val);
+							fwrite(&val,sizeof(int),1,arch);
+							prueb=prueb+sizeof(int);
+						}
+						if(atrib.tipo=='f')
+						{
+							printf("Dame el valor flotante:");
+							scanf("%f",&val1);
+							fwrite(&val1,sizeof(float),1,arch);
+							prueb=prueb+sizeof(float);
+						}
+						if(atrib.tipo=='c')
+						{
+							printf("Dame el valor entero:");
+							scanf("%s", val2);
+							fwrite(val2,sizeof(atrib.tam)+1,1,arch);
+							prueb=prueb+sizeof(val2);
+						}
+						if(atrib.tipo=='l')
+						{
+							printf("Dame el valor long:");
+							scanf("%ld",&val3);
+							fwrite(&val3,sizeof(long),1,arch);
+							prueb=prueb+sizeof(long);
+						}
+						if(atrib.tipo=='b')
+						{
+							printf("Dame el valor booleano 1 o 0:");
+							scanf("%d",&op);
+							val4=(op==1);
+							fwrite(&val4,sizeof(bool),1,arch);
+							prueb=prueb+sizeof(bool);
+						}
+						if(atrib.puntsig==-1)
+						{
+							break;
+						}
+						fseek(arch,atrib.puntsig,SEEK_SET);
+					}
+					return;
+				}
+				else
+				{
+					fseek(arch,arr[op-1],SEEK_SET);
+					fread(&prueb,sizeof(long),1,arch);
+					fseek(arch,entidad.puntatri,SEEK_SET);
+					while(fread(&atrib,sizeof(Atri),1,arch)==1)
+					{
+						fseek(arch,prueb,SEEK_SET);
+						pos=ftell(arch);
+						if(atrib.tipo=='i')
+						{
+							printf("Dame el valor entero:");
+							scanf("%d",&val);
+							fwrite(&val,sizeof(int),1,arch);
+							prueb=prueb+sizeof(int);
+						}
+						if(atrib.tipo=='f')
+						{
+				
+							printf("Dame el valor flotante:");
+							scanf("%f",&val1);
+							fwrite(&val1,sizeof(float),1,arch);
+							prueb=prueb+sizeof(float);
+						}
+						if(atrib.tipo=='c')
+						{
+							
+							printf("Dame el valor entero:");
+							scanf("%s", val2);
+							fwrite(val2,sizeof(atrib.tam)+1,1,arch);
+							prueb=prueb+sizeof(val2);
+						}
+						if(atrib.tipo=='l')
+						{
+							
+							printf("Dame el valor long:");
+							scanf("%ld",&val3);
+							fwrite(&val3,sizeof(long),1,arch);
+							prueb=prueb+sizeof(long);
+						}
+						if(atrib.tipo=='b')
+						{
+							printf("Dame el valor booleano 1 o 0:");
+							scanf("%d",&op);
+							val4=(op==1);
+							fwrite(&val4,sizeof(bool),1,arch);
+							prueb=prueb+sizeof(bool);
+						}
+						if(atrib.puntsig==-1)
+						{
+							break;
+						}
+						fseek(arch,atrib.puntsig,SEEK_SET);
+					}
+					return;
+				}
+			}
+			cab=entidad.puntsig;
+		}
+		printf("No se encontro\n");
 }
